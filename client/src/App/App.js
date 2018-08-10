ef76f39d97555a9bf277f1df8af65aaf28eac23a
Barbara Rodriguez <barbs2089@gmail.com>
2018-08-10 11:46:00 +1000

    Setup client folder structure

---
+++ client/src/App/App.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import Home from '../Home';
+import Navbar from '../Navbar';
+import './app.css';
+
+export default App;
---
+++ client/src/Home/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Login from './Login';
+import Register from './Register';
+
+export default Home;
