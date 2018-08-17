6e2d1a4c59d897e8fe78dfa14d8df705690a25e1
Michael Winter <mcwinter07@gmail.com>
2018-08-17 13:55:54 +1000

    rudimentrary ch ch changes

--- client/src/widget/carouselWidget/WidgetCarousel.js
+++ client/src/widget/carouselWidget/WidgetCarousel.js
@@ -3,7 +3,6 @@ import Slider from './slider';
 import '../WidgetPage.css'
 
 
-<<<<<<< HEAD
 export default (props) => {
   const params = {
     pagination: '.swiper-pagination',
@@ -15,32 +14,6 @@ export default (props) => {
       prevEl: '.swiper-button-prev',
     },
     loop: true
-=======
-class WidgetCarousel extends React.Component {
-
-  render() {
-    const params = {
-      centeredSlides: true,
-      paginationClickable: true,
-      spaceBetween: 30,
-      setWrapperSize: true,
-      direction: 'horizontal',
-      }
-
-    return(
-      <div>
-        <Swiper {...params}>
-          <div>Slide 1</div>
-          <div>Slide 2</div>
-          <div>Slide 3</div>
-          <div>Slide 4</div>
-          <div>Slide 5</div>
-          <div>Slide 6</div>
-        </Swiper>
-
-      </div>
-    )
->>>>>>> 96794ff6e04a6334e1eaefd396e3e9c3eae8fd81
   }
 
   return(
