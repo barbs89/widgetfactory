4297813c3f9e464051616af9da249548690a7336
Michael Winter <mcwinter07@gmail.com>
2018-08-30 01:23:12 +1000

    form modification stored to local cache (onlick) intialised with state and rendered(onlick). need automation and clleaning

--- client/src/widget/WidgetPage.js
+++ client/src/widget/WidgetPage.js
@@ -15,111 +15,46 @@ class WidgetPage extends React.Component {
     
     const localkeys = Object.keys(localStorage)
     const localvalues = Object.values(localStorage)
-    
-    const statePopulator = (locKey, locVal, object) => {
-      locKey.forEach((key,value) => object[key] = locVal[value])
-    }
-          
-    let finalobject = {}
-    localkeys.forEach((key,value) => finalobject[key] = localvalues[value])
+    const initialisedSate = {}
 
-    console.log(finalobject)
-    // let finalKey = Object.keys(finalobject)
+    localkeys.forEach((key,value) => initialisedSate[key] = localvalues[value])
 
-    // const filter = (object) => {
-    //   object.forEach((key,value) => this.state[key] = object[value])
-    // }
-
-    // let variable = []
-    // for (let i = 0; i < localStorage.length; ++i) {
-    //   let variable = {[localkeys[i]]: localvalues[i]}
-    //   // let object = JSON.parse(variable)
-    //   // console.log(variable)
-    //   // console.log(variable[i])
-    //   let result = Object.assign({}, Object.keys(variable): Object.values(variable))
-    //     // , ...Object.values(variable)
-      
-    //   let stringifiedJSON = JSON.stringify(variable)
-    //   let parsedObject = JSON.parse(stringifiedJSON)
-    //   console.log(result)
-    //   // let singleObj = {...{[localkeys[i]]: localvalues[i]}}
-    //   // console.log(allObj)
-      
-
-    //   // console.log(allObj)
-    //   // let obj1 = {[localkeys[i]]: localvalues[i]}
-    //   // let obj2 = Object.assign({}, obj1)
-    //   // array.push(obj)
-    //   // console.log(obj2)
-    //   // console.log(localvalues[i])
-    //   // let cache = {[localkeys[i]]: localvalues[i]}
-    //   // let object = Object.assign(cache)
-    //   // console.log(object)      
-    // }
-    
-    // const picked = (({ finalKey }) => ({ finalobject }))(finalobject)
-    // console.log(picked)
-    this.state = finalobject
-      
+    console.log(initialisedSate)
+    this.state = initialisedSate 
   }
-    
+// Multiple Singe Arrays into a single object
+  // const arraysToObject = (locKey, locVal, object) => {
+  //   locKey.forEach((key,value) => object[key] = locVal[value])
+  // }
   
-
-  // state = {
-  //   // titleCard: '',
-  //   // priceCard: '',
-  //   // descCard: '',
-  //   // ctaCard: '',
-  //   // callToActionDesc: '',
-  //   // borderOutline: '',
-  //   // borderStyle: '',
-  //   // cardColor: '',
-  //   // backgroundColor: '',
-  //   // buttonColor: '',
-  //   // font: '',
-  //   // fontStyle: '',
-  //   // fontColor: '',
-  //   // fontSize: '',
-  //   // fontAlignment: ''
-  // }
-
-  // createVariables = (key, value) => {
-  //   for (let i = 0; localStorage.length; ++i) {
-  //       variable[i] = {[key]: value};
-  //   }
-  //   return variable;
-  // }
-
-// Form Handlers //
-
 //Handle Input Change
-handleFormChange = (e) => {
-  const target = e.target
-  const value = target.type === 'checkbox' ? target.checked : target.type === 'radio' ? target.value : target.value
-  const name = target.name
-  e.preventDefault()
-  console.log(`Key is ${name} with a value of ${value}`)
-  this.setState({
-    [name]: value,
-  })
-}
+  handleFormChange = (e) => {
+    const target = e.target
+    const value = target.type === 'checkbox' ? target.checked : target.type === 'radio' ? target.value : target.value
+    const name = target.name
+    e.preventDefault()
+    console.log(`Key is ${name} with a value of ${value}`)
+    this.setState({
+      [name]: value,
+    })
+  }
 
 // Handle Checkbox Inputs
-handleShowCheckbox = (e) => {
-  const target = e.target
-  const name = target.name
-  if(target.checked === true) {
-    this.setState({ 
-      [name]: target.value = 'visible'
-    })
-    console.log(this.state)
-  } else if(target.checked === false) {
-    this.setState({ 
-      [name]: target.value = 'hidden'
-    })
-    console.log(this.state)
+  handleShowCheckbox = (e) => {
+    const target = e.target
+    const name = target.name
+    if(target.checked === true) {
+      this.setState({ 
+        [name]: target.value = 'visible'
+      })
+      console.log(this.state)
+    } else if(target.checked === false) {
+      this.setState({ 
+        [name]: target.value = 'hidden'
+      })
+      console.log(this.state)
+    }
   }
-}
   handleCheckboxChange = (e) => {
     const target = e.target
     const name = target.name
@@ -130,9 +65,7 @@ handleShowCheckbox = (e) => {
       });
     };
   }
-
-  // Handle Checkbox Inputs
-
+// Handle Checkbox Inputs
   handleShowCheckbox = (e) => {
     const target = e.target;
     const name = target.name;
@@ -149,7 +82,6 @@ handleShowCheckbox = (e) => {
       console.log(this.state);
     }
   };
-
   handleCheckboxChange = (e) => {
     const target = e.target;
     const name = target.name;
@@ -166,9 +98,7 @@ handleShowCheckbox = (e) => {
       console.log(this.state);
     }
   };
-
-  // Handle Radio Inputs
-
+// Handle Radio Inputs
   handleRadioChange = (e) => {
     const target = e.target;
     const name = target.name;
@@ -177,8 +107,6 @@ handleShowCheckbox = (e) => {
   })
     console.log(this.state)
   }
-
-
 // Handle Radio Inputs
   handleRadioChange = (e) => {
     const target = e.target
@@ -190,7 +118,6 @@ handleShowCheckbox = (e) => {
     console.log(name, value, target)
     console.log(this.state)
   }
-
 // Handle Colour Input
   handleFormColorChange = (e) => {
     const target = e.target
@@ -203,7 +130,6 @@ handleShowCheckbox = (e) => {
       [name]: rgbVal,
     })
   }
-
 // Convert Hexadecimal to RGB 
   hexToRgb = (hex) => {
     let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -214,7 +140,6 @@ handleShowCheckbox = (e) => {
     }
     return `rgb(${Object.values(value).join(`, `)})`
   }
-
 // Handle Form Submission
   handleFormSubmit = (e) => {
     e.preventDefault()
@@ -226,7 +151,6 @@ handleShowCheckbox = (e) => {
     })
     console.log(this.state)
   }
-
 // Render Widget
   updateOutputProperty = (e) => {
     e.preventDefault()
@@ -238,23 +162,30 @@ handleShowCheckbox = (e) => {
       document.documentElement.style.setProperty(`--${keys[i]}`, values[i])
     }
     console.log(this.state)
+  }  
+  
+  arraysToObject = (locKey, locVal, object) => {
+    locKey.forEach((key,value) => object[key] = locVal[value])
   }
+  
 
-
-  updatelocalcache = (e) => {
+// Update the Local Storage
+  updatelocalStorage = (e) => {
     e.preventDefault()
     const keys = Object.keys(this.state)
     const values = Object.values(this.state)
-    console.log(keys)
-    console.log(values)
-    // const localkeys = Object.keys(localStorage)
-    // const localvalues = Object.values(localStorage)
-    const storageLength = Storage.length
+    const storageLength = values.length
+
     for (let i = 0; i < storageLength; i++) {
+      console.log([keys[i]], values[i])
       localStorage.setItem([keys[i]], values[i]);
-      // console.log(localkeys, localvalues);
+      
     }
+    // localStorage.clear()
   }
+  
+
+
 
   updateIframe = () => {
     document.getElementById('iframeRefresher').addEventListener('click', function() {
@@ -291,7 +222,7 @@ handleShowCheckbox = (e) => {
             handleShow={this.handleShowCheckbox}
           />
           <button onClick={this.updateOutputProperty}>Preview Widget</button>
-          <input onClick={this.updatelocalcache} type='submit' defaultValue='Save to Cache'/>
+          <input onClick={this.updatelocalStorage} type='submit' defaultValue='Save to Cache'/>
         </section>
 
         <section className="widget-render-section">
